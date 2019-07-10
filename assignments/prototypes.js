8ff93c5f7400260a29778f12ef0a3f1b96aceed9
krboelter <krboelter@gmail.com>
2019-07-09 19:35:43 -0600

    still working on stretch

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -17,7 +17,7 @@ function GameObject(attributes) {
 }
 
 GameObject.prototype.destroy = function () {
-  console.log(`${this.name} was removed from the game`);
+  return `${this.name} was removed from the game`;
 };
 // * createdAt
 // * name
@@ -34,7 +34,9 @@ function CharacterStats (stats) {
 
 CharacterStats.prototype = Object.create (GameObject.prototype);
 CharacterStats.prototype.takeDamage = function () {
-  return `${this.name} took damage.`;
+  console.log(`${this.name} took damage.`);
+  // this.healthPoints -= 5;
+  // console.log(this.healthPoints);
 }
 
 
@@ -62,6 +64,26 @@ Humanoid.prototype.greet = function () {
 // * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
 // * should inherit destroy() from GameObject through CharacterStats
 // * should inherit takeDamage() from CharacterStats
+
+// *****Stretch task:*****
+// * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
+// * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
+// * Create two new objects, one a villain and one a hero and fight it out with methods!
+
+//===Hero===
+// function Hero (heroAtt) {
+//   Humanoid.call(this, heroAtt);
+// }
+
+//Hero.prototype = Object.create(Humanoid.prototype);
+
+
+//===Villan===
+// function Villan (villAtt) {
+//   Humanoid.call(this. villAtt)
+// }
+
+//Villan.prototype = Object.create(Humanoid.prototype);
 
  
 /*
@@ -123,6 +145,39 @@ const archer = new Humanoid({
   language: 'Elvish',
 });
 
+// const cptAmerica = new Hero({
+//   createdAt: new Date(),
+//   dimensions: {
+//     length: 1,
+//     width: 2,
+//     height: 6,
+//   },
+//   healthPoints: 15,
+//   name: 'Captain America',
+//   team: 'America',
+//   weapons: [
+//     'shield',
+//     'Super Strength Drugs',
+//   ],
+//   language: 'English'
+// });
+
+// const redSkull = new Villan({
+//   createdAt: new Date(),
+//   dimensions: {
+//     length: 1,
+//     width: 1.5,
+//     height: 5,
+//   },
+//   healthPoints: 5,
+//   name: 'Red Skull',
+//   team: 'Hydra',
+//   weapons: [
+//     'gun'
+//   ],
+//   language: 'German'
+// });
+
 console.log(mage.createdAt); // Today's date
 console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
 console.log(swordsman.healthPoints); // 15
@@ -133,9 +188,6 @@ console.log(archer.language); // Elvish
 console.log(archer.greet()); // Lilith offers a greeting in Elvish.
 console.log(mage.takeDamage()); // Bruce took damage.
 console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+// console.log(redSkull.greet())
 
 
-  // Stretch task: 
-  // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
-  // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
-  // * Create two new objects, one a villain and one a hero and fight it out with methods!
